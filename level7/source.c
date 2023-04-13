576acdf2b4787b1ff6c06236502ca4fce6c193b5
lobbyra <jecaudal@student.42.fr>
2023-04-13 05:14:24 -0400

    add level7

---
+++ level7/source.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char c[80];
+
+void m() {
+	printf(printf("%s - %d\n", c, time(0));)
+}
+
+int main(int argc, char **argv) {
+	void *b1 = malloc(8);
+	*b1 = (int)1;
+	*(b1 + 4) = malloc(8);
+	void *b2 = malloc(8);
+	*b2 = (int)2;
+	*(b2 + 4) = malloc(8);
+	strcpy(*(b1 + 4), argv[1]);
+	strcpy(*(b2 + 4), argv[2]);
+	int fd = fopen("/home/user/level8/.pass", data.080486e9);
+    	fgets(c, 0x44, uVar2);
+    	puts("~~");
+	return ;
+}
